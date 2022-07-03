0b5c862aa3d28290f8de83c8c958c8371241d91a
Subrina Islam <subrinaislam29@gmail.com>
2022-07-03 13:10:43 +1000

    Update Experience.js

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -30,19 +30,18 @@ export const Experience = () => {
           </h1>
           <h2>Web Developer Intern at RSL, Tasmania from September2020 to December 2020)</h2>
           <p 
-          className="chatting-description"
+          className="Web Developer Intern at RSL"
           style={{
             textAlign: "left",
             color: "rgb(168, 164, 164)",
             backgroundColor: "#141414",
           }}
           >
-          Web Developer Intern at RSL, Tasmania from September2020 to December 2020)
-          ● Maintained the current website and added more appealing features.
-          ● Redesigned sections of the website to ensure a more attractive and user-friendly experience.
-          ● Created custom HTML emails for clients/in-house newsletters and invitations for distribution to our databases.
-          ● Enable security measures in the current website.
-          ● Plan and make progress in order to make the current website into an appealing one.
+             ● Maintained the current website and added more appealing features.
+             ● Redesigned sections of the website to ensure a more attractive and user-friendly experience.
+             ● Created custom HTML emails for clients/in-house newsletters and invitations for distribution to our databases.
+             ● Enable security measures in the current website.
+             ● Plan and make progress in order to make the current website into an appealing one.
  
           </p>
         </Col>
